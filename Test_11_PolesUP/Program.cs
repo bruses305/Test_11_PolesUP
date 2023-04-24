2ba4a21b402bf6e95741966abfab9df47130144f
37533 <37533@LAPTOP-GAM2DSJ2>
2023-04-24 21:48:05 +0300

    Добавьте файлы проекта.

---
+++ Test_11_PolesUP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Test_11_PolesUP
+{
+    class Program
+    {
+        static int[] SortAscending(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 };
+            arr = SortAscending(arr);
+            for(int i = 0; i<arr.Length;i++)
+            {
+                Console.Write(arr[i]+",");
+            }
+        }
+    }
+}
